6acd9854f9adb0ee47bfad8af0337b1e5f68ac22
Viktor Nikitin <vnikitin90@gmail.com>
2020-06-14 18:39:43 -0500

    Update test_perf.py

--- tests/test_perf.py
+++ tests/test_perf.py
@@ -2,7 +2,8 @@ import numpy as np
 import usfftrecon as pt
 from timing import *
 if __name__ == "__main__":
-# Performance tests on multi gpus    
+# Performance tests on several gpus    
+
     # Model parameters
     n = 2448  # object size in x,y
     nz = 2048  # object size in z
@@ -11,9 +12,11 @@ if __name__ == "__main__":
     theta = np.linspace(0, np.pi, ntheta).astype('float32')  # angles
     pnz = 16  # number of complex64 slices in each partition for simultaneous processing on each gpu, 
               # should be less or equal than (nz/ngpus/2) because each 2 float32 slices are coupled into 1 complex64
-    # Load object
+    # init some data
     data = np.zeros([ntheta,nz,n],dtype='float32')
     data[:,:,n//4:3*n//4]=1
+    
+    # check times for different number of gpus
     for ngpus in range(1,5):
         # Class gpu solver
         with pt.SolverTomo(theta, nz, n, pnz, center, ngpus) as slv:
